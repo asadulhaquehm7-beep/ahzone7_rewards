deb4fac0b3405b90328bf53b619a22c1db37e329
asadulhaquehm7-beep <asadulhaquehm7@gmail.com>
2026-02-22 12:38:16 +0600

    Delete firebase.js
    Signed-off-by: asadulhaquehm7-beep <asadulhaquehm7@gmail.com>

--- firebase.js
+++
@@ -1,8 +0,0 @@
-const admin = require("firebase-admin");
-
-admin.initializeApp({
-  credential: admin.credential.cert(require("./firebase-key.json"))
-});
-
-const db = admin.firestore();
-module.exports = db;
